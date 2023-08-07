291a9273582d9c4bd590d3a0cbadfb5ad432b38e
adarshUC <adarshuc595@gmail.com>
2023-08-07 18:52:57 +0530

    2

--- Music/helpers/strings.py
+++ Music/helpers/strings.py
@@ -63,7 +63,7 @@ class TEXTS:
     SOURCE = (
         "**Source 📦:**\n\n"
         "**Note:** \n__» The source code is available on GitHub. You can find the link below.__\n"
-        "__» Every project available under The-HellBot are open-source and free to use and modify to your needs.__\n"
+        "__» Every project available under TEAM TOFANI e open-source and free to use and modify to your needs.__\n"
         "__» Anyone pretending to be the developer of this bot and selling the code, is a scammer.__\n\n"
         "__» Please consider giving a star to the repository if you liked the project.__\n"
         "__» Feel free to contact us if you need any help regarding the source code.__\n\n"
@@ -77,7 +77,7 @@ class TEXTS:
         "    __Blocked Users:__ `{3} users`\n"
         "    __Songs Played:__ `{4} songs`\n"
         "    __Active VC:__ `{5} vc`\n\n"
-        "**⤞ System Stats:**\n"
+        "**⤞ Systum Stats:**\n"
         "    __Core:__ `{6} cores`\n"
         "    __CPU Usage:__ `{7}`\n"
         "    __Disk Usage:__ `{8}`\n"
@@ -212,4 +212,4 @@ class TEXTS:
         "__» Enjoy the music and let us know if you have any suggestions for improvement.__\n\n"
         "❤️🎶 @{2}"
     )
-    PERFORMER = "[ †hê Hêllẞø† ]"
+    PERFORMER = "[ TEAM TOFANI ]"
